d93e958068731210d79b3dbbd28e1b5e1f02413a
Meowkai's Laptop <verioserio@gmail.com>
2021-08-18 16:39:09 +0200

    add a gradient coloring of squares from white to black

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function randomValue(){
 function createDiv(){
     div = document.createElement("div");
     div.classList.add("flexItem");
+    div.classList.add("white");
     div.addEventListener("mouseenter", color);
 
     return div;
@@ -18,14 +19,43 @@ function createDiv(){
 function addDiv(n){
     for(i = 0; i < n; i++){
         for( j = 0; j < n; j++){
-            flexbox.appendChild(createDiv());
+            flexbox.appendChild(createDiv());   
         }
     }
 }
 
 function color(e){
-
-    e.target.style.backgroundColor = `rgb(${randomValue()},${randomValue()},${randomValue()})`;
+    if(e.target.classList.contains("white")){
+        e.target.classList.remove("white");
+        e.target.classList.add("black90");
+    }else if(e.target.classList.contains("black90")){
+        e.target.classList.remove("black90");
+        e.target.classList.add("black80");
+    }else if(e.target.classList.contains("black80")){
+        e.target.classList.remove("black80");
+        e.target.classList.add("black70");
+    }else if(e.target.classList.contains("black70")){
+        e.target.classList.remove("black70");
+        e.target.classList.add("black60");
+    }else if(e.target.classList.contains("black60")){
+        e.target.classList.remove("black60");
+        e.target.classList.add("black50");
+    }else if(e.target.classList.contains("black50")){
+        e.target.classList.remove("black50");
+        e.target.classList.add("black40");
+    }else if(e.target.classList.contains("black40")){
+        e.target.classList.remove("black40");
+        e.target.classList.add("black30");
+    }else if(e.target.classList.contains("black30")){
+        e.target.classList.remove("black30");
+        e.target.classList.add("black20");
+    }else if(e.target.classList.contains("black20")){
+        e.target.classList.remove("black20");
+        e.target.classList.add("black10");
+    }else if(e.target.classList.contains("black10")){
+        e.target.classList.remove("black10");
+        e.target.classList.add("black");
+    }
     
 }
 
@@ -51,6 +81,6 @@ const flexbox = document.querySelector(".flex");
 const button = document.querySelector("button");
 
 addDiv(16);
-randomValue();
+
 
 button.addEventListener("click", erase);
